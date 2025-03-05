1c240651cee4b546e98b24c662c184176e0c2ced
jeevangb <jeevanindian01@gmail.com>
2025-03-05 13:35:57 +0530

    #7: spun up grpc server

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -1,1 +1,37 @@
 package configs
+
+import (
+	"github.com/go-playground/validator"
+	"github.com/rs/zerolog/log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port string `mapstructure:"GRPC_SERVER_PORT"`
+}
+
+var envs = []string{
+	"GRPC_SERVER_PORT",
+}
+
+func LoadConfig(env *string) (Config, error) {
+	var cfg Config
+	viper.AddConfigPath("./")
+	viper.SetConfigFile("internal/configs/" + *env + "/application.env")
+	viper.ReadInConfig()
+	for _, env := range envs {
+		if err := viper.BindEnv(env); err != nil {
+			log.Error().Err(err).Msg("could not bind envs")
+			return cfg, err
+		}
+	}
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Error().Err(err).Msg("could not unmarshal the configs")
+		return cfg, err
+	}
+	if err := validator.New().Struct(&cfg); err != nil {
+		log.Error().Err(err).Msg("could not validate the config struct")
+	}
+	return cfg, nil
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,25 @@
 package server
+
+import (
+	"net"
+
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+func RegisterGrpcServer(port string) error {
+	//Initialize the lister
+	listner, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Error().Err(err).Msg("error while listening port")
+	}
+	//create new grpc server instance
+	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(50 * 1024 * 1024))
+	log.Info().Msg("portal management service listening on port: 50051")
+	
+	err = grpcServer.Serve(listner)
+	if err != nil {
+		log.Error().Err(err).Msg("error while running grpc  server")
+	}
+	return nil
+}
